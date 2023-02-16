e3ed42eb4cf388bf1aa7b927b99c6033045b6063
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2023-02-16 16:32:49 -0500

    adding github api token

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+
 function UserResults() {
-  return (
-    <div>UserResults</div>
-  )
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}user`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+    const data = await response.json();
+  };
+
+  return <div>UserResults</div>;
 }
 
-export default UserResults
+export default UserResults;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import UserResults from "../components/users/UserResults";
 function Home() {
   return (
     <>
+    {/* SEARCH COMPONENT GOES HERE */}
+
       <UserResults />
     </>
   );
