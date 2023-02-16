beafce817bad5187b8130b246ffc2cf81199ccc1
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2023-02-16 12:07:24 -0500

    testing github api

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,8 +8,7 @@ function NotFound() {
         <div className='max-w-lg'>
           <h1 className='text-8xl font-bold mb-8'>Oops!</h1>
           <p className='text-5xl mb-8'>404 - Page not found!</p>
-          <Link to='/'
-          className='btn btn-primary btn-lg'>
+          <Link to='/' className='btn btn-secondary btn-lg'>
             <FaHome className='mr-2' />
             Back To Home
           </Link>
