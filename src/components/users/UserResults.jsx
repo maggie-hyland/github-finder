a10ed8aef9b6a3521a313ac9bc33121883d9b030
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2023-02-23 10:53:50 -0500

    updated

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
 
-//not showing user data when console logging
-
 function UserResults() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const github_url = process.env.REACT_APP_GITHUB_URL + `/users`;
+  const github_token = `token ` + process.env.REACT_APP_GITHUB_TOKEN;
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    const response = await fetch(github_url, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: github_token,
       },
     });
 
@@ -23,11 +23,17 @@ function UserResults() {
     setLoading(false);
   };
 
-  return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      User Results
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>
+  }
 }
 
 export default UserResults;
